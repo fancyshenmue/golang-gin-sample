54186cda9ae503a0995b3f90c39dbb7531f756eb
Charles Hsu <charles@handsup.shop>
2020-09-13 14:00:59 +0800

    update consul-api

--- cmd/consul-api/api/get.go
+++ cmd/consul-api/api/get.go
@@ -14,12 +14,12 @@ import (
 // @version 1.0
 // @accept  json
 // @produce json
-// @param params body GetConsulKeyTop true "body"
+// @param params body ConsulGetKeyTop true "body"
 // @Success 200 string string
 // @Router /api/v1/getKey [post]
 func GetKey(c *gin.Context) {
 	var (
-		reqBody GetConsulKeyTop
+		reqBody ConsulGetKeyTop
 		resBody CommonInterface
 	)
 
@@ -39,6 +39,48 @@ func GetKey(c *gin.Context) {
 	}
 
 	json.Unmarshal(pair.Value, &resBody)
+
+	c.JSON(http.StatusOK, resBody)
+}
+
+// ListKey | List Key
+// @Summary List Key
+// @Id 1
+// @Tags List Key
+// @version 1.0
+// @accept  json
+// @produce json
+// @param params body ConsulAPIListKeyTop true "body"
+// @Success 200 string string
+// @Router /api/v1/listKey [post]
+func ListKey(c *gin.Context) {
+	var (
+		reqBody      ConsulAPIListKeyTop
+		resBodySlice CommonStringSlice
+		resBody      CommonMap
+	)
+
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Get a handle to the KV API
+	kv := consulClient.KV()
+
+	// Get Key List
+	pair, _, err := kv.List(reqBody.Key, nil)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	for _, v := range pair {
+		resBodySlice = append(resBodySlice, v.Key)
+	}
+
+	resBody = make(map[string]interface{})
+	resBody["keyList"] = resBodySlice
 
 	c.JSON(http.StatusOK, resBody)
 }
